9fe0d0836204da250d88a24cb76854834235f10d
David Passarelli <dpassarelli@camelotcg.com>
2021-01-21 21:53:51 -0500

    Comments & refactoring.

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const { DateTime } = require('luxon')
 
-function getFirstDayOfWeek (dt, basedOn) {
-  /*
-    ISC License
-
-    Copyright (c) 2021, David Passarelli <dpassarelli@camelotcg.com>
-
-    Permission to use, copy, modify, and/or distribute this software for any
-    purpose with or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+/**
+ * Returns an instance of DateTime that represents the first Monday on or before
+ * the specified date. This can be changed to return the first Sunday instead.
+ *
+ * @param  {Date|DateTime}   dt      The date to start from.
+ *
+ * @param  {String?}         basis   If set to 'sunday', then the day
+ *                                   immediately preceding the first Monday on
+ *                                   or before the specified date will be
+ *                                   returned instead. Any other value will be
+ *                                   ignored.
+ *
+ * @return {DateTime}
+ */
+function getFirstDayOfWeek (dt, basis) {
+  /**
+   * @license
+   * Copyright (c) 2021, David Passarelli <dpassarelli@camelotcg.com>
+   *
+   * Permission to use, copy, modify, and/or distribute this software for any
+   * purpose with or without fee is hereby granted, provided that the above
+   * copyright notice and this permission notice appear in all copies.
+   *
+   * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
    */
-
-  if (dt == null) {
-    throw new Error('The first parameter is required and must be a JavaScript Date object, or an instance of luxon\'s DateTime class.')
-  }
-
-  if (!(dt instanceof Date) && !DateTime.isDateTime(dt)) {
-    throw new Error('The first parameter is required and must be a JavaScript Date object, or an instance of luxon\'s DateTime class.')
-  }
 
   if (dt instanceof Date) {
     dt = DateTime.fromJSDate(dt)
   }
 
-  basedOn = basedOn || 'monday'
+  if (!DateTime.isDateTime(dt)) {
+    throw new Error('The first parameter is required and must be a JavaScript Date object, or an instance of luxon\'s DateTime class.')
+  }
 
-  if (basedOn.toLowerCase() === 'sunday') {
+  basis = basis || 'monday'
+
+  if (basis.toLowerCase() === 'sunday') {
     return dt.startOf('week').minus({ day: 1 })
   }
 
